e9647d03371d0127bb07e7d0bda87ff4e0643bad
dimmerz92 <andrew.weymes@outlook.com>
2022-11-29 18:39:47 +0800

    add project class

---
+++ src/classes/project.js
@@ -0,0 +1,21 @@
+import item from "./item";
+
+const project = (title, desc) => {
+    const items = [];
+
+    const getProject = () => {
+        return {title, desc};
+    };
+
+    const newItem = (itemTitle, itemDesc, date, priority) => {
+        items.push(item(itemTitle, itemDesc, date, priority));
+    };
+
+    const deleteItem = (index) => {
+        items.splice(index, 1);
+    };
+
+    return {getProject, newItem, deleteItem}
+};
+
+export default project;
