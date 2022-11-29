c4dd741dfa42a0ec50b09cfa369e0e4ce9fe1686
dimmerz92 <andrew.weymes@outlook.com>
2022-11-29 18:31:40 +0800

    add item class

---
+++ src/classes/item.js
@@ -0,0 +1,15 @@
+const item = (title, desc, date, priority) => {
+    const getItem = () => {
+        return {title, desc, date, priority};
+    };
+
+    const changeDate = (newDate) => {
+        date = newDate;
+    };
+
+    const changePriority = (newPriority) => {
+        priority = newPriority;
+    };
+
+    return {getItem, changeDate, changePriority};
+};
