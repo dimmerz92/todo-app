01f4180265981424bd9cb49b7a251ad93466f9e3
dimmerz92 <andrew.weymes@outlook.com>
2022-11-29 20:09:06 +0800

    add listener for project button

---
+++ src/classes/listener.js
@@ -0,0 +1,25 @@
+import about from "../content-layouts/about";
+import contact from "../content-layouts/contact";
+import home from "../content-layouts/home"
+import newProject from "../content-layouts/newProject";
+import render from "./render"
+
+const listener = (handler) => {
+    // nav links
+    document.getElementById('home').addEventListener('click', () => {
+        render(home(), 'mainContent');
+    });
+    document.getElementById('about').addEventListener('click', () => {
+        render(about(), 'mainContent');
+    });
+    document.getElementById('contact').addEventListener('click', () => {
+        render(contact(), 'mainContent');
+    });
+
+    // project button
+    document.getElementById('newProject').addEventListener('click', () => {
+        render(newProject(handler), 'mainContent');
+    });
+};
+
+export default listener;
