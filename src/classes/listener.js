96313bff9242dbfd5f128286876bb2b61fe96188
dimmerz92 <andrew.weymes@outlook.com>
2022-11-29 20:42:36 +0800

    modify project button listener

--- src/classes/listener.js
+++ src/classes/listener.js
@@ -2,6 +2,7 @@ import about from "../content-layouts/about";
 import contact from "../content-layouts/contact";
 import home from "../content-layouts/home"
 import newProject from "../content-layouts/newProject";
+import projectView from "../content-layouts/projectView";
 import render from "./render"
 
 const listener = (handler) => {
@@ -19,6 +20,7 @@ const listener = (handler) => {
     // project button
     document.getElementById('newProject').addEventListener('click', () => {
         render(newProject(handler), 'mainContent');
+        render(projectView(handler.getProject(-1)), 'mainContent');
     });
 };
 
