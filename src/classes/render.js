18ea4fb2c30c3bd7530e25afef91730abc0d65d2
dimmerz92 <andrew.weymes@outlook.com>
2022-11-30 13:05:28 +0800

    fix array handling

--- src/classes/render.js
+++ src/classes/render.js
@@ -1,7 +1,11 @@
 const render = (content, target) => {
     const view = document.getElementById(target);
     view.innerHTML = '';
-    for (let i = 0; i < content.length; i++) {
+    if (content.length > 1) {
+        for (let i = 0; i < content.length; i++) {
+            view.appendChild(content);
+        };
+    } else {
         view.appendChild(content);
     };
 };
