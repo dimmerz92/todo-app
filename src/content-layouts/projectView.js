9cf70cf8de6acc3e16d40d0b0b08a67ede902f11
dimmerz92 <andrew.weymes@outlook.com>
2022-11-30 19:18:01 +0800

    add item display to project view layout

--- src/content-layouts/projectView.js
+++ src/content-layouts/projectView.js
@@ -1,16 +1,37 @@
+import render from "../classes/render";
+import itemCard from "./itemCard";
+import newItem from "./newItem";
+
 const projectView = (project) => {
+    // content container
     const content = document.createElement('div');
     content.id = 'projectViewLayout';
 
+    // header
     const h1 = document.createElement('h1');
     h1.textContent = project.title;
     content.appendChild(h1);
 
+    // description
     const desc = document.createElement('p');
     desc.textContent = project.desc;
     content.appendChild(desc);
 
-    // get item cards
+    // new item
+    const itemButton = document.createElement('button');
+    itemButton.textContent = 'New Item';
+    itemButton.addEventListener('click', () => {
+        render(newItem(project), 'mainContent');
+    });
+    content.appendChild(itemButton);
+
+    // item cards 
+    const items = document.createElement('div');
+    items.classList.add('itemCardContainer');
+    project.items.forEach(item => {
+        items.appendChild(itemCard(item.getItem()));
+    });
+    content.appendChild(items);
 
     return content;
 };
