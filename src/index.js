9f3489e0549f6e94bd7772677dc5254bc9c9ff27
dimmerz92 <andrew.weymes@outlook.com>
2022-11-30 13:12:21 +0800

    test cohesion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import handler from './classes/handler';
+import listener from './classes/listener';
+import render from './classes/render';
+import home from './content-layouts/home';
 import './style.css';
+
+const myHandler = handler();
+
+listener(myHandler);
+
+render(home(), 'mainContent');
