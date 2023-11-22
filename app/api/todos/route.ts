8f2e3df1c43804d091057bb2d5e16f607bfbb388
Andrew <andrew.weymes@outlook.com>
2023-11-22 22:06:51 +0800

    handle errors

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createTodoSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1).max(255),
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().max(1000, "Description exceeds 1000 characters"),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createTodoSchema.safeParse(body);
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
   const newTodo = await prisma.todo.create({
--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 interface TodoForm {
@@ -13,27 +13,39 @@ interface TodoForm {
 const NewTodoPage = () => {
   const router = useRouter();
   const { register, handleSubmit } = useForm<TodoForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/todos", data);
-        router.push("/todos");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input
-          placeholder="Add new tasks..."
-          {...register("title")}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/todos", data);
+            router.push("/todos");
+          } catch (error) {
+            setError("An unexpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input
+            placeholder="Add new tasks..."
+            {...register("title")}
+          />
+        </TextField.Root>
+        <TextArea
+          placeholder="Add any details about your task..."
+          {...register("description")}
         />
-      </TextField.Root>
-      <TextArea
-        placeholder="Add any details about your task..."
-        {...register("description")}
-      />
-      <Button>Submit</Button>
-    </form>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
