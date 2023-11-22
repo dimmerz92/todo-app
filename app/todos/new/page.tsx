09e0a288aa5908d18874de00ce9edfb63f8f83f5
Andrew <andrew.weymes@outlook.com>
2023-11-22 21:54:01 +0800

    handle form submit

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,16 +1,39 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import React from "react";
+import axios from "axios";
+
+interface TodoForm {
+  title: string;
+  description: string;
+}
 
 const NewTodoPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<TodoForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/todos", data);
+        router.push("/todos");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Add new tasks..." />
+        <TextField.Input
+          placeholder="Add new tasks..."
+          {...register("title")}
+        />
       </TextField.Root>
-      <TextArea placeholder="Add any details about your task..." />
+      <TextArea
+        placeholder="Add any details about your task..."
+        {...register("description")}
+      />
       <Button>Submit</Button>
-    </div>
+    </form>
   );
 };
 
